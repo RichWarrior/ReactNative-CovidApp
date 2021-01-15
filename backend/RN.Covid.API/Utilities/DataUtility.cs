564cdef9b57770789937f819caef99ffd30007c0
unknown <faruk_thecno@hotmail.com>
2021-01-15 10:17:56 +0300

    Bug Fixed

--- backend/RN.Covid.API/Utilities/DataUtility.cs
+++ backend/RN.Covid.API/Utilities/DataUtility.cs
@@ -18,38 +18,41 @@ namespace RN.Covid.API.Utilities
                 HtmlWeb web = new HtmlWeb();
                 var doc = web.Load(BASE_URL);
                 var scripts = doc.DocumentNode.Descendants("script");
-                var currentScript = (scripts).LastOrDefault(x => x.InnerText.Contains("geneldurumjson"));
+                var currentScript = (scripts).LastOrDefault(item => item.InnerText.Contains("geneldurumjson"));
                 if (currentScript != null)
                 {
-                    var startIndex = currentScript.InnerText.IndexOf('{') - 1;
-                    var endIndex = currentScript.InnerText.LastIndexOf(';');
-                    if (startIndex > -1 && endIndex > -1)
+                    var startIndeitem = currentScript.InnerText.IndexOf('{') - 1;
+                    var endIndeitem = currentScript.InnerText.LastIndexOf(';');
+                    if (startIndeitem > -1 && endIndeitem > -1)
                     {
-                        var json = currentScript.InnerText.Substring(startIndex, endIndex - startIndex);
+                        var json = currentScript.InnerText.Substring(startIndeitem, endIndeitem - startIndeitem);
                         var dummyModel = JsonConvert.DeserializeObject<List<DummyCovidModel>>(json);
-                        result = dummyModel.Select(x => new RNCovidModel
+                        foreach (var item in dummyModel)
                         {
-                            Date = DateTime.Parse(x.tarih.Replace(".", "/")),
-                            DailyTest = String.IsNullOrEmpty(x.gunluk_test) ? 0 : decimal.Parse(x.gunluk_test),
-                            DailyCase = String.IsNullOrEmpty(x.gunluk_vaka) ? 0 : decimal.Parse(x.gunluk_vaka),
-                            DailySick = String.IsNullOrEmpty(x.gunluk_hasta) ? 0 : decimal.Parse(x.gunluk_hasta),
-                            DailyDeath = String.IsNullOrEmpty(x.gunluk_vefat) ? 0 : decimal.Parse(x.gunluk_vefat),
-                            DailyHealing = String.IsNullOrEmpty(x.gunluk_iyilesen) ? 0 : decimal.Parse(x.gunluk_iyilesen),
-                            TotalTest = String.IsNullOrEmpty(x.toplam_test) ? 0 : decimal.Parse(x.toplam_test),
-                            TotalSick = String.IsNullOrEmpty(x.toplam_hasta) ? 0 : decimal.Parse(x.toplam_hasta),
-                            TotalDeath = String.IsNullOrEmpty(x.toplam_vefat) ? 0 : decimal.Parse(x.toplam_vefat),
-                            TotalIntensiveCare = String.IsNullOrEmpty(x.toplam_yogun_bakim) ? 0 : decimal.Parse(x.toplam_yogun_bakim),
-                            TotalIntubated = String.IsNullOrEmpty(x.toplam_entube) ? 0 : decimal.Parse(x.toplam_entube),
-                            SickPneumonioRate = String.IsNullOrEmpty(x.hastalarda_zaturre_oran) ? 0 : decimal.Parse(x.hastalarda_zaturre_oran),
-                            SeriouslySickCount = String.IsNullOrEmpty(x.agir_hasta_sayisi) ? 0 : decimal.Parse(x.agir_hasta_sayisi),
-                            BedOccupancyRate = String.IsNullOrEmpty(x.yatak_doluluk_orani) ? 0 : decimal.Parse(x.yatak_doluluk_orani),
-                            IntensiveCareOccupancy = String.IsNullOrEmpty(x.eriskin_yogun_bakim_doluluk_orani) ? 0 : decimal.Parse(x.eriskin_yogun_bakim_doluluk_orani),
-                            VentilatorOccupancyRate = String.IsNullOrEmpty(x.ventilator_doluluk_orani) ? 0 : decimal.Parse(x.ventilator_doluluk_orani),
-                            AverageFilmingTime = String.IsNullOrEmpty(x.ortalama_filyasyon_suresi) ? 0 : decimal.Parse(x.ortalama_filyasyon_suresi),
-                            AverageContactDetectionTime = x.ortalama_temasli_tespit_suresi,
-                            FilmingRate = String.IsNullOrEmpty(x.filyasyon_orani) ? 0 : decimal.Parse(x.filyasyon_orani)
-
-                        }).ToList();
+                            item.tarih = item.tarih.Replace(".", "/");
+                            var pieces = item.tarih.Split('/');
+                            var model = new RNCovidModel();
+                            model.Date = new DateTime(int.Parse(pieces[2]), int.Parse(pieces[1]),int.Parse(pieces[0]), 0, 0, 0);
+                            model.DailyTest = String.IsNullOrEmpty(item.gunluk_test) ? 0 : decimal.Parse(FormatNumber(item.gunluk_test));
+                            model.DailyCase = String.IsNullOrEmpty(item.gunluk_vaka) ? 0 : decimal.Parse(FormatNumber(item.gunluk_vaka));
+                            model.DailySick = String.IsNullOrEmpty(item.gunluk_hasta) ? 0 : decimal.Parse(FormatNumber(item.gunluk_hasta));
+                            model.DailyDeath = String.IsNullOrEmpty(item.gunluk_vefat) ? 0 : decimal.Parse(FormatNumber(item.gunluk_vefat));
+                            model.DailyHealing = String.IsNullOrEmpty(item.gunluk_iyilesen) ? 0 : decimal.Parse(FormatNumber(item.gunluk_iyilesen));
+                            model.TotalTest = String.IsNullOrEmpty(item.toplam_test) ? 0 : decimal.Parse(FormatNumber(item.toplam_test));
+                            model.TotalSick = String.IsNullOrEmpty(item.toplam_hasta) ? 0 : decimal.Parse(FormatNumber(item.toplam_hasta));
+                            model.TotalDeath = String.IsNullOrEmpty(item.toplam_vefat) ? 0 : decimal.Parse(FormatNumber(item.toplam_vefat));
+                            model.TotalIntensiveCare = String.IsNullOrEmpty(item.toplam_yogun_bakim) ? 0 : decimal.Parse(FormatNumber(item.toplam_yogun_bakim));
+                            model.TotalIntubated = String.IsNullOrEmpty(item.toplam_entube) ? 0 : decimal.Parse(FormatNumber(item.toplam_entube));
+                            model.SickPneumonioRate = String.IsNullOrEmpty(item.hastalarda_zaturre_oran) ? 0 : decimal.Parse(FormatNumber(item.hastalarda_zaturre_oran));
+                            model.SeriouslySickCount = String.IsNullOrEmpty(item.agir_hasta_sayisi) ? 0 : decimal.Parse(FormatNumber(item.agir_hasta_sayisi));
+                            model.BedOccupancyRate = String.IsNullOrEmpty(item.yatak_doluluk_orani) ? 0 : decimal.Parse(FormatNumber(item.yatak_doluluk_orani));
+                            model.IntensiveCareOccupancy = String.IsNullOrEmpty(item.eriskin_yogun_bakim_doluluk_orani) ? 0 : decimal.Parse(FormatNumber(item.eriskin_yogun_bakim_doluluk_orani));
+                            model.VentilatorOccupancyRate = String.IsNullOrEmpty(item.ventilator_doluluk_orani) ? 0 : decimal.Parse(FormatNumber(item.ventilator_doluluk_orani));
+                            model.AverageFilmingTime = String.IsNullOrEmpty(item.ortalama_filyasyon_suresi) ? 0 : decimal.Parse(FormatNumber(item.ortalama_filyasyon_suresi));
+                            model.AverageContactDetectionTime = item.ortalama_temasli_tespit_suresi;
+                            model.FilmingRate = String.IsNullOrEmpty(item.filyasyon_orani) ? 0 : decimal.Parse(FormatNumber(item.filyasyon_orani));
+                            result.Add(model);
+                        }
                     }
                 }
             }
@@ -58,6 +61,26 @@ namespace RN.Covid.API.Utilities
                 Console.WriteLine(ex.Message);
             }
             return result;
+        }
+
+        public static string FormatNumber(string number)
+        {
+            var pieces = number.Split('.');
+            if (pieces.Length > 2)
+            {
+                string result = "";
+                for (var i = 0; i < pieces.Length; i++)
+                {
+                    if (i == pieces.Length - 1)
+                    {
+                        result += "." + pieces[i];
+                    }
+                    else
+                        result += pieces[i];
+                }
+                return result;
+            }
+            return number;
         }
     }
 }
